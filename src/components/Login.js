caf7c8af52bb05b168b71faea9fa8d3d7b0ffc4d
t0nylombardi <alombardi.331@gmail.com>
2024-05-30 14:34:44 -0400

    code along login feat

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,10 @@
+// Login.js
 import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
 function Login() {
+  // Access the login function passed as context
+  const login = useOutletContext();
   const [formData, setFormData] = useState({
     username: "",
     password: "",
@@ -13,13 +17,15 @@ function Login() {
     });
   }
 
+  // Create a function that calls the login function when the form is submitted
   function handleLogin(e) {
     e.preventDefault();
+    login();
   }
 
   return (
     <form onSubmit={handleLogin}>
-      <label for="username">Username</label>
+      <label htmlFor="username">Username</label>
       <div>
         <input
           id="username"
@@ -29,7 +35,7 @@ function Login() {
           onChange={handleChange}
         />
       </div>
-      <label for="password">Password</label>
+      <label htmlFor="password">Password</label>
       <div>
         <input
           id="password"
@@ -37,7 +43,7 @@ function Login() {
           name="password"
           value={formData.password}
           onChange={handleChange}
-        />      
+        />
       </div>
       <button type="submit">Login</button>
     </form>
